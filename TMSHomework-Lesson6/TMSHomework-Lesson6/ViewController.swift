b4c46668e424f55914cb9d1ae234c1696ce6b099
Natallia Mazur <natashamazur@bk.ru>
2023-12-05 22:38:17 +0300

    Homework tasks added

---
+++ TMSHomework-Lesson6/TMSHomework-Lesson6/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TMSHomework-Lesson6
+//
+//  Created by Наталья Мазур on 5.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    // TODO: 1. Функция вычисления факториала числа - через передачу массива в параметр функции. Массив из 6 чисел -> Факториал числа \(numbers), равен ????
+
+    // TODO: 2. var x = [(1, "x"), (4, "y"), (6, «a»), (9, «u»), (5, «k»)] Массив из кортежей - возвести Int в квадрат
+    // TODO: 3. отфильтровать только четные Int -> [4, 6]
+    // TODO: 4. упорядочить по строкам по возрастанию -> [a, k, u, x, y]
+
+    // TODO: 5. написать список книг и отфильтровать книги по жанрам -> horror = [] var books = ["Book001": ["title": "Swift Programming", "author": "John Doe", "genre": "Programming"]]
+
+    // TODO: 6. объеденить 2 массива. первый массив это название групп. второй название песен
+
+    // TODO: 7. [2, nil, 4, nil, 3, 8, nil] - не используя compactMap. Создать функцию которая принимает массив чисел и умножает каждое число на 2 используя функцию map.
+        
+    }
+
+
+}
+
