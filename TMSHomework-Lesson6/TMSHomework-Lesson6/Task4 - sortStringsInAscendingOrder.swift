a333613f63e180da9e048075ac1d41be0e65d758
Natallia Mazur <natashamazur@bk.ru>
2023-12-08 14:46:14 +0300

    4th task done, 2nd task fixed

--- TMSHomework-Lesson6/TMSHomework-Lesson6/Task4 - sortStringsInAscendingOrder.swift
+++ TMSHomework-Lesson6/TMSHomework-Lesson6/Task4 - sortStringsInAscendingOrder.swift
@@ -8,3 +8,7 @@
 import Foundation
 
 // TODO: 4. упорядочить по строкам по возрастанию -> [a, k, u, x, y]
+func sortStringsInAscendingOrder(arrayOfTuples: [(Int, String)]) -> [(Int, String)] {
+    let sortedArray = arrayOfTuples.sorted(by: {$0.1 < $1.1})
+    return sortedArray
+}
