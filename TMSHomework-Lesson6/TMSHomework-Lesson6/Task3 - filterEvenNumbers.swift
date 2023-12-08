8b10186c456748d42e71a1f039789a7efc113f4c
Natallia Mazur <natashamazur@bk.ru>
2023-12-08 14:31:00 +0300

    2nd and 3rd tasks done

--- TMSHomework-Lesson6/TMSHomework-Lesson6/Task3 - filterEvenNumbers.swift
+++ TMSHomework-Lesson6/TMSHomework-Lesson6/Task3 - filterEvenNumbers.swift
@@ -7,4 +7,9 @@
 
 import Foundation
 
-// TODO: 3. отфильтровать только четные Int -> [4, 6] 
+// TODO: 3. отфильтровать только четные Int -> [4, 6] в массиве var x = [(1, "x"), (4, "y"), (6, «a»), (9, «u»), (5, «k»)] 
+
+func filterEvenNumbers(arrayOfTuples: [(Int, String)]) -> [(Int, String)]{
+    let filteredArray = arrayOfTuples.filter({$0.0 % 2 == 0})
+    return filteredArray
+}
