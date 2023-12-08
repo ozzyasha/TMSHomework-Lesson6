c4c46cc0bf614f3db61a10e7340ab20b28e788c5
Natallia Mazur <natashamazur@bk.ru>
2023-12-08 23:58:55 +0300

    7th task done

--- TMSHomework-Lesson6/TMSHomework-Lesson6/Task7 - squareUsingMap.swift
+++ TMSHomework-Lesson6/TMSHomework-Lesson6/Task7 - squareUsingMap.swift
@@ -7,4 +7,13 @@
 
 import Foundation
 
+var arrayWithNil = [2, nil, 4, nil, 3, 8, nil]
+
 // TODO: 7. [2, nil, 4, nil, 3, 8, nil] - не используя compactMap. Создать функцию которая принимает массив чисел и умножает каждое число на 2 используя функцию map.
+func squareUsingMap(array: [Int?]) -> [Int] {
+    
+    var arrayWithoutNil = array
+    arrayWithoutNil.removeAll(where: {$0 == nil})
+    let squaredArray = arrayWithoutNil.map({($0!)*($0!)})
+    return squaredArray
+}
